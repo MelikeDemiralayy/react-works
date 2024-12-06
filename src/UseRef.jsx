e332f2fdff300b44d7d145e1cb6d743b1cdcbfd6
mdemiralay <m.demiralay@vitalen.com.tr>
2024-12-06 17:15:05 +0300

    fixed

--- src/UseRef.jsx
+++ src/UseRef.jsx
@@ -1,21 +1,25 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 const UseRef = () => {
- let [number,setNumber] = useState(0);
-
+//  let [number,setNumber] = useState(0);
+const ref = useRef(2);
+console.log(ref);
  useEffect(() =>{
-    console.log("COMPONENT RENDERED");
- })
+    console.log("COMPONENT RENDERED"); // her render sonrasında useEffect çalışır
+ },[])
 
  function handleClick(){
-    setNumber(n => n+1);
+   //  setNumber(n => n+1);
+   // ref.current = ref.current+1;
+   ref.current++;
+   console.log(ref.current)
  }
 
   return (
     <button onClick={handleClick} className='button-ref' >
         Click Me!
     </button>
-  )
+  ) 
 }
 
 export default UseRef
